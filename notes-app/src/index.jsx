c07640a6c830825365976aa1db8ffd4f370eaff7
Dian Tri Budianto <dian.budianto@fastratabuana.co.id>
2024-11-22 17:03:15 +0700

    remove

--- notes-app/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import NoteApp from './components/NoteApp.jsx'
-
-const root = createRoot(document.getElementById('root'))
-root.render(
-    <NoteApp/>
-)
